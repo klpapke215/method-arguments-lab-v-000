54aa9a912d3fbc00b692c5b0fa8f4075c48ecae7
Kristen Papke <klpapke215@gmail.com>
2018-06-28 13:40:03 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,8 +4,8 @@ end
 
 introduction("Josh")
 
-def introduction_and_language(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_and_language("Dan", "Ember.js")
+introduction_with_language("Dan", "Ember.js")
